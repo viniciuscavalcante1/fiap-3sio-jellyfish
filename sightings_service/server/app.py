150bd50f53e391203aa4d625990a290d28040b95
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2024-06-05 23:31:43 -0300

    feat(sightings): Add app

--- sightings_service/server/app.py
+++ sightings_service/server/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from auth_service.server.routers.auth import router as auth_router
+from sightings_service.server.routers.sightings import router as sightings_router
 from pathlib import Path
 from fastapi.templating import Jinja2Templates
 
@@ -8,7 +8,7 @@ templates = Jinja2Templates(directory='templates')
 
 app = FastAPI()
 
-app.include_router(auth_router, tags=["auth"], prefix="/auth")
+app.include_router(sightings_router, tags=["sightings"], prefix="/sightings")
 
 app.mount(
     "/static",
