a1fa1d31b998ba9874d9f4820ab7d60de5c1a6e0
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2024-06-05 21:41:42 -0300

    feat: Add jwt auth

--- auth_service/server/routers/auth.py
+++ auth_service/server/routers/auth.py
@@ -1,15 +1,20 @@
+from datetime import timedelta
+
 from fastapi import APIRouter, Request, status, Form, HTTPException, Depends
 from fastapi.responses import RedirectResponse
 from starlette.templating import Jinja2Templates
 from ..database import get_db_connection
+from ...utils.utils import create_access_token, verify_token
 from psycopg2.extras import RealDictCursor
 import hashlib
+
+SECRET_KEY = "jellyfish"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 router = APIRouter()
 templates = Jinja2Templates(directory='templates')
 
-usuario_autenticado = False
-email_usuario = None
 
 def autenticar_usuario(email, senha):
     conn = get_db_connection()
@@ -34,11 +39,15 @@ def login_get(request: Request):
 
 @router.post("/login")
 def login_post(request: Request, email: str = Form(...), senha: str = Form(...)):
-    global usuario_autenticado, email_usuario
     usuario_autenticado, email_usuario = autenticar_usuario(email, senha)
     # TODO: alterar response
     if usuario_autenticado:
-        response = RedirectResponse(url=f"/index", status_code=status.HTTP_302_FOUND)
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(
+            data={"sub": email_usuario}, expires_delta=access_token_expires
+        )
+        response = RedirectResponse(url=f"/auth/index", status_code=status.HTTP_302_FOUND)
+        response.set_cookie(key="access_token", value=f"Bearer {access_token}", httponly=True)
         return response
     else:
         raise HTTPException(
@@ -71,9 +80,14 @@ def register_post(request: Request, email: str = Form(...), senha: str = Form(..
     response = RedirectResponse(url=f"/auth/login", status_code=status.HTTP_302_FOUND)
     return response
 
-@router.get("/dashboard")
+@router.get("/index")
 def dashboard(request: Request):
-    if not usuario_autenticado:
+    token = request.cookies.get("access_token")
+    if not token:
         raise HTTPException(status_code=401, detail="Não autorizado")
+    payload = verify_token(token.split(" ")[1])
+    if not payload:
+        raise HTTPException(status_code=401, detail="Não autorizado")
+    email_usuario = payload.get("sub")
     context = {'request': request, 'email': email_usuario}
-    return templates.TemplateResponse('dashboard.html', context)
+    return {"Eita, deu certo!"}
